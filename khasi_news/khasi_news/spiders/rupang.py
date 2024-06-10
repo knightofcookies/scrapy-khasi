0dcfd96509dca2c798d1349633cb98f3a4c238e9
notacrosswalk <121627492+notacrosswalk@users.noreply.github.com>
2024-06-10 20:06:58 +0530

    Add crawled rupang data

--- khasi_news/khasi_news/spiders/rupang.py
+++ khasi_news/khasi_news/spiders/rupang.py
@@ -14,7 +14,8 @@ class RupangSpider(scrapy.Spider):
         yield scrapy.Request(url=url, callback=self.parse, meta={"playwright": True})
 
     def parse(self, response):
-        links: List[str] = response.css("table tbody tr td a ::attr(href)").extract()
+        selector = scrapy.Selector(text=response.text)
+        links: List[str] = selector.css("table tbody tr td a ::attr(href)").extract()
         for link in links:
             if "post-sitemap" in link:
                 yield scrapy.Request(
@@ -22,15 +23,16 @@ class RupangSpider(scrapy.Spider):
                 )
 
     def parse_sitemap(self, response):
-        links = response.css("table tbody tr td a ::attr(href)").extract()
+        selector = scrapy.Selector(text=response.text)
+        links = selector.css("table tbody tr td a ::attr(href)").extract()
         for link in links:
-            # yield SeleniumRequest(url=link, callback=self.parse_article_page)
             yield scrapy.Request(
                 url=link, callback=self.parse_article_page, meta={"playwright": True}
             )
 
     def parse_article_page(self, response):
-        te = response.css("main.content p::text").extract()
+        selector = scrapy.Selector(text=response.text)
+        te = selector.css("main.content p::text").extract()
         te_processed = []
         for item in te:
             for i in item.split("\n"):
@@ -39,7 +41,7 @@ class RupangSpider(scrapy.Spider):
 
         title_selector = """body > div.site-container > div
          > div > main > article > header > h1 ::text"""
-        title = response.css(title_selector).get()
+        title = selector.css(title_selector).get()
 
         if article_text is not None and article_text != "":
             news_item = KhasiNewsItem()
