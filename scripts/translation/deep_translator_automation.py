3950c6d8963dcfa586ed7961f51965b66d523dd1
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-07-05 17:38:33 +0530

    Bug fixes

--- scripts/translation/deep_translator_automation.py
+++ scripts/translation/deep_translator_automation.py
@@ -11,17 +11,20 @@ async def main() -> None:
     if len(sys.argv) < 2:
         return
 
-    with open(f"to_translate{sys.argv[1]}", "r", encoding="utf-8") as f:
+    with open(f"to_translate/{sys.argv[1]}.txt", "r", encoding="utf-8") as f:
         orig_lines = f.readlines()
 
     file_path = f"translated/{sys.argv[1]}_kha_to_en.txt"
     if os.path.exists(file_path):
         os.remove(file_path)
 
+    if not os.path.exists("translated/"):
+        os.mkdir("translated")
+
     kha_to_en_translator = GoogleTranslator(source="en", target="kha")
     for line in orig_lines:
         txt = kha_to_en_translator.translate(line)
-        with open("kha_to_en.txt", "a", encoding="utf-8") as f:
+        with open(file_path, "a", encoding="utf-8") as f:
             f.write(txt + "\n")
 
 
