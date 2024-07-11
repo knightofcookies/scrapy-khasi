7b3482f07b8dfe9ea418e98358b5ab69defdd627
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-07-11 17:14:33 +0530

    Add parts 1 to 10

--- scripts/translation/deep_translator_automation.py
+++ scripts/translation/deep_translator_automation.py
@@ -2,21 +2,31 @@ import winsound
 import datetime
 import os
 import asyncio
-import sys
+import pickle
+import concurrent.futures
+from typing import List
 from deep_translator import GoogleTranslator
 
 # Add {"khasi": "kha"} to the Google Translate dict in constants.py in deep_translator
 
+THREAD_LIMIT = 25
 
-async def main() -> None:
-    if len(sys.argv) < 2:
-        return
+START = 1  # Delete pickle_dump if you change this
+END = 100  # Delete pickle_dump if you change this
 
-    with open(f"chunks_for_translation/{sys.argv[1]}.txt", "r", encoding="utf-8") as f:
+PICKLE_DUMP_PATH = "pickle_dump"
+
+
+def translate_chunk(chunk: str, complete_index: int) -> None:
+
+    source = "kha"
+    target = "en"
+
+    with open(f"chunks_for_translation/{chunk}.txt", "r", encoding="utf-8") as f:
         orig_lines = f.readlines()
 
     index = 0
-    file_path = f"translated/{sys.argv[1]}_kha_to_en.txt"
+    file_path = f"translated/{chunk}_{source}_to_{target}.txt"
     if os.path.exists(file_path):
         with open(file_path, "r", encoding="utf-8") as f:
             index = len(f.readlines())
@@ -26,14 +36,40 @@ async def main() -> None:
     if not os.path.exists("translated/"):
         os.mkdir("translated")
 
-    kha_to_en_translator = GoogleTranslator(source="kha", target="en")
+    translator = GoogleTranslator(source=source, target=target)
 
     while index < len(orig_lines):
         line = orig_lines[index]
-        txt = kha_to_en_translator.translate(line)
-        with open(file_path, "a", encoding="utf-8") as f:
-            f.write(txt + "\n")
+        txt = translator.translate(line)
+        if txt is not None:
+            with open(file_path, "a", encoding="utf-8") as f:
+                f.write(txt + "\n")
         index += 1
+
+    with open(PICKLE_DUMP_PATH, "rb+") as fp:
+        complete = pickle.load(fp)
+        complete[complete_index] = True
+        pickle.dump(complete, fp)
+
+
+async def main() -> None:
+    if not os.path.exists(PICKLE_DUMP_PATH):
+        complete: List[bool] = [False * (END - START + 1)]
+        with open(PICKLE_DUMP_PATH, "wb") as fp:
+            pickle.dump(complete, fp)
+
+    with open(PICKLE_DUMP_PATH, "rb") as fp:
+        complete = pickle.load(fp)
+
+    pool = concurrent.futures.ThreadPoolExecutor(max_workers=THREAD_LIMIT)
+
+    chunk_index = 0
+    while chunk_index < (END - START + 1):
+        if not complete[chunk_index]:
+            pool.submit(translate_chunk, f"part{START+chunk_index}.txt", chunk_index)
+        chunk_index += 1
+
+    pool.shutdown(wait=True)
 
 
 if __name__ == "__main__":
