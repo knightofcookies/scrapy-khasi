d542f0d5c394f2e7beb33ce62e667a5ef7abccb5
Lopen Lamora <101050984+knightofcookies@users.noreply.github.com>
2024-08-15 15:39:32 +0530

    Minor bug fix

--- wikipedia/break_tsv.py
+++ wikipedia/break_tsv.py
@@ -3,16 +3,19 @@ import os
 with open("unified.tsv", "r", encoding="utf-8") as fp:
     lines = fp.readlines()
 
-if os.path.exists("en_custom.txt"):
-    os.remove("en_custom.txt")
+if os.path.exists("custom.tsv"):
+    os.remove("custom.tsv")
 
-if os.path.exists("kha_custom.txt"):
-    os.remove("kha_custom.txt")
+# if os.path.exists("kha_custom.txt"):
+#     os.remove("kha_custom.txt")
 
-for i in range(200001, 500001):
-    line = lines[i]
-    en_line, kha_line = line.split("\t\t\t\t\t")
-    with open("en_custom.txt", "a", encoding="utf-8") as fp:
-        fp.write(en_line.strip() + "\n")
-        with open("kha_custom.txt", "a", encoding="utf-8") as fp2:
-            fp2.write(kha_line.strip() + "\n")
+with open("custom.tsv", "a", encoding="utf-8") as fp:
+    fp.writelines(lines[200001:500001])
+
+# for i in range(200001, 500001):
+#     line = lines[i]
+#     en_line, kha_line = line.split("\t\t\t\t\t")
+    # with open("en_custom.txt", "a", encoding="utf-8") as fp:
+    #     fp.write(en_line.strip() + "\n")
+    #     with open("kha_custom.txt", "a", encoding="utf-8") as fp2:
+    #         fp2.write(kha_line.strip() + "\n")
--- wikipedia/generate_unified_dataset.py
+++ wikipedia/generate_unified_dataset.py
@@ -5,8 +5,9 @@ with open("../scripts/translation/cleaned_parallel_corpus.tsv", "r", encoding="u
 with open("translated_parallel_corpus_2.tsv", "r", encoding="utf-8") as fp:
     lines_2 = fp.readlines()
 
-lines = lines_1 + lines_2
+lines = lines_1[1:] + lines_2[1:]
 lines = sorted(lines, key=len)
 
 with open("unified.tsv", "w", encoding="utf-8") as fp:
+    fp.write("en\t\t\t\t\tkha\n")
     fp.writelines(lines)
